7235ab9fa9a2fa9985414a9bb25165f1b08b6d94
Jeremiah Etim <jeremiahetim.97@gmail.com>
2026-01-05 07:26:07 -0800

    Update vite config with base path

--- everrise-frontend/vite.config.js
+++ everrise-frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This ensures your assets (CSS/Images) load correctly on GitHub Pages
+  base: '/EveriseTower/', 
 })
